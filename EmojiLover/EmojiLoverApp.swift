852ac1359382c82fc153377ef3938d1ca8d42836
zeeshanayaz <zeeshanayaz1@gmail.com>
2023-09-27 16:44:04 +0500

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by muhammad.zeeshan on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
