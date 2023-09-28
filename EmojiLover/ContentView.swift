5997a8dbd20fb6592bad70e8e8417f7b836cc2ce
zeeshanayaz <zeeshanayaz1@gmail.com>
2023-09-28 10:30:43 +0500

    preview added

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+enum Emoji : String, CaseIterable {
+    case 😀, 😍, 🤠, 🙌
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😀
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){ emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Emoji Lovers!
+
+## Demo Preview
+
+<img src="https://github.com/zeeshanayaz/emoji_lovers_ios_development/raw/main/demo/emoji_lovers_preview.gif" height="600" />
